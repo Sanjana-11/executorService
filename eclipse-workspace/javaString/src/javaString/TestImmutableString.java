650e652843f84d57822d45bebf2467d43f3bb5e1
Sanjana Prakash <68433048+Sanjana-11@users.noreply.github.com>
2023-03-30 16:15:37 +0530

    Delete eclipse-workspace/javaString/src/javaString directory
    deleted unwanted files

--- eclipse-workspace/javaString/src/javaString/TestImmutableString.java
+++
@@ -1,9 +0,0 @@
-package javaString;
-
-public class TestImmutableString {
-	public static void main(String[] args) {
-		String s = "Sachin";
-		s = s.concat("Tendulkar");
-		System.out.println(s);
-	}
-}
